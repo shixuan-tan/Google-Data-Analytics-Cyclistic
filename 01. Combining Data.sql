44df5a246ea4b759f4f740f6f848fb6599b3d709
Tan Shi Xuan <seesuant@gmail.com>
2025-07-30 21:14:57 +0800

    Create 01. Combining Data.sql

---
+++ 01. Combining Data.sql
@@ -0,0 +1,32 @@
+-- To combine the 12 monthly data tables into one table consisting of all biketrip data from Jan 2024 to Dec 2024. 
+
+CREATE TABLE `project2-460706.Cyclistic.2024_data` AS
+SELECT* FROM `project2-460706.Cyclistic.202401-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202402-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202403-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202404-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202405-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202406-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202407-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202408-data` 
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202409-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202410-data`
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202411-data` 
+UNION ALL
+SELECT* FROM `project2-460706.Cyclistic.202412-data` 
+;
+
+-- The combined table contains 5,860,568 rows. 
+
+SELECT COUNT(*)
+FROM `project2-460706.Cyclistic.2024_data`;
